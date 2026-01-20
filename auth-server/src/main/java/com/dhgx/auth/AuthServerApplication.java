97de22f29a9edff225e2c51a9e9b266b8bc2c08a
liyan <liyan2@elht.com>
2026-01-20 14:18:17 +0800

    提交

--- auth-server/src/main/java/com/dhgx/auth/AuthServerApplication.java
+++ auth-server/src/main/java/com/dhgx/auth/AuthServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(AuthProperties.class)
-@MapperScan("com.example.mngsys.auth.mapper")
+@MapperScan("com.dhgx.auth.mapper")
 /**
  * AuthServerApplication。
  */
